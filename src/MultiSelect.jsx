f1aa9a7b5e8437760ba08ac25b2383f1debe64cf
mo2009453 <mando12mohamed@gmail.com>
2025-05-15 01:20:32 +0300

    MultiSelect.jsx

--- src/MultiSelect.jsx
+++ src/MultiSelect.jsx
@@ -1,1 +1,41 @@
+import React, { useState } from 'react';
 
+export default function MultiSelect({ options, selectedOptions, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOption = (option) => {
+    if (selectedOptions.includes(option)) {
+      onChange(selectedOptions.filter((o) => o !== option));
+    } else {
+      onChange([...selectedOptions, option]);
+    }
+  };
+
+  return (
+    <div className="relative">
+      <div
+        className="border p-2 rounded cursor-pointer bg-white"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {selectedOptions.length > 0
+          ? selectedOptions.join(', ')
+          : 'اختر الأجهزة المتخصصة'}
+      </div>
+      {isOpen && (
+        <div className="absolute z-10 bg-white border rounded mt-1 w-full max-h-48 overflow-y-auto">
+          {options.map((option) => (
+            <div
+              key={option}
+              className={`p-2 cursor-pointer hover:bg-gray-100 ${
+                selectedOptions.includes(option) ? 'bg-gray-200 font-semibold' : ''
+              }`}
+              onClick={() => toggleOption(option)}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
