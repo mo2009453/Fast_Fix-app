2f682c59e3e01ed4c10c5b122f6dbf9ed2e7812d
mo2009453 <mando12mohamed@gmail.com>
2025-05-14 23:32:13 +0300

    supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,6 @@
-// src/lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://Fast_Fix-app.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyemR4cmVzbWJxcnZwZnh6d2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MDE2NDMsImV4cCI6MjA2MjI3NzY0M30.-Yf_AEmy8XF0j-t7Xw34mL__3U5FpzJBjv2nWzegetk'
+const supabaseUrl = 'https://yrzdxresmbqrvpfxzwfv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyemR4cmVzbWJxcnZwZnh6d2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MDE2NDMsImV4cCI6MjA2MjI3NzY0M30.-Yf_AEmy8XF0j-t7Xw34mL__3U5FpzJBjv2nWzegetk';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
