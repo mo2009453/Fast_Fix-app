2937c822cdc77a900c5355ea046a6ed71be60c0f
mo2009453 <mando12mohamed@gmail.com>
2026-06-10 20:00:19 +0300

    supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,5 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://bcxeoisgixdlccgsquet.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjeGVvaXNnaXhkbGNjZ3NxdWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MzY3NzksImV4cCI6MjA5NjUxMjc3OX0.SADGItfMWy1cQJUuJS7B0gVnvvuU276o8_XCjGixWxM';
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
